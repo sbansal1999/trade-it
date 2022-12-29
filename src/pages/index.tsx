6201e249838949d09ccbf02d1826fe1e12b020fe
sbansal1999 <sbansal1999@gmail.com>
2022-12-29 16:56:25 +0530

    add search form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,47 @@
-import { trpc } from "../utils/trpc";
+import Head from "next/head";
+import { createRef, FormEvent, useRef } from "react";
 
-export default function IndexPage() {
-  const mutation = trpc.users.createUser.useMutation();
+const fetchData = (searchQuery: string) => {};
 
-  const data = {
-    password: "Bansal@23",
-    firstName: "Satyam",
-    lastName: "Bansal",
-    dateOfBirth: "1999-08-23T00:00:00Z",
-    email: "sbansal1999@gmail.com",
+export default function Home() {
+  const searchRef = createRef<HTMLInputElement>();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!searchRef.current) return;
+
+    const searchQuery = searchRef.current.value;
   };
-
-  const handleRegister = async () => {
-    mutation.mutate(data);
-  };
-
-  if (mutation.error)
-    return <div>Something went wrong {mutation.error.message}</div>;
-
-  if (mutation.isLoading) return <div>Loading data...</div>;
 
   return (
     <>
-      <div>
-        <button onClick={handleRegister}>Register</button>
-        <div>
-          <h3>
-            User Data:
-            {JSON.stringify(data)}
-          </h3>
-        </div>
+      <Head>
+        <title>Trade IT</title>
+      </Head>
 
-        <div>
-          <h3 className="text-red-500">
-            Response from API:
-            {JSON.stringify(mutation.data)}
-          </h3>
+      <main>
+        <div className="w-fw mt-10 flex justify-center">
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span className="text-md font-medium text-slate-700">
+                Stock Name
+              </span>
+              <div className="mt-3" />
+              <input
+                type="text"
+                placeholder="AAPL"
+                ref={searchRef}
+                className="text-md placeholder-grey-500 disbled focus:ring-slate-400focus:invalid:border-pink-500 mt-1 block w-full rounded-md border-2
+                border-slate-400 bg-white p-2 focus:border-sky-500  focus:outline-none focus:ring-1"
+              />
+            </label>
+            <div className="m-5" />
+            <button className="w-full rounded-md bg-blue-400 py-3 text-white hover:bg-blue-900">
+              <span className="text-3xl">SEARCH</span>
+            </button>
+          </form>
         </div>
-        <button className="rounded bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-          ABC
-        </button>
-      </div>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/index.tsx"],
+  content: ["./src/pages/*.{ts,js,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      body: ['"Open Sans"'],
+    },
   },
   plugins: [],
 };
