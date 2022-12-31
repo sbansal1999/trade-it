392a28f4336506c74d365530e5add86119040a01
sbansal1999 <sbansal1999@gmail.com>
2022-12-31 14:25:49 +0530

    remove daisyui :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/*.{ts,js,jsx,tsx}"],
-  daisyui: {
-    themes: ["light"],
+  theme: {
+    extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
