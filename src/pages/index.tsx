c66d63e89cf7c687715dbb853b18354b17a84dc0
sbansal1999 <sbansal1999@gmail.com>
2023-01-01 23:15:06 +0530

    remove button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -183,14 +183,7 @@ const ShowStocksModal = ({
                       className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                       onClick={() => setShowModal(false)}
                     >
-                      Deactivate
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Cancel
+                      Go Back
                     </button>
                   </div>
                 </Dialog.Panel>
