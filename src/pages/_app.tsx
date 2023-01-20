cd9345ee0c2df080dcca399b308475a56cf44140
sbansal1999 <sbansal1999@gmail.com>
2023-01-20 20:10:03 +0530

    add tanstack query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,18 @@ import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
