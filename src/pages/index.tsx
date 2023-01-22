d4b24316b37e53b01c82a7f57681f61aa907af93
sbansal1999 <sbansal1999@gmail.com>
2023-01-22 12:19:13 +0530

    add confirm order button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -111,12 +111,21 @@ export default function Home() {
       return;
     }
 
-    const orderData = {
-      ticker: selectedStock.ticker,
+    if (!userData) {
+      return;
+    }
+
+    const latestQuote = await fetchStockQuote(selectedStock.ticker);
+
+    tradeMutation.mutate({
+      symbol: selectedStock.ticker,
       quantity,
-      price: selectedStockQuote.c,
-      brokerage,
-    };
+      price: latestQuote.c,
+      userId: userData.id,
+      action: "BUY",
+    });
+
+    setShowConfirmOrderModal(false);
   };
 
   return (
@@ -335,7 +344,7 @@ export default function Home() {
                           </div>
                         </div>
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                        <button
+                          <button
                             type="button"
                             className="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                             onClick={handleConfirmOrder}
@@ -349,7 +358,6 @@ export default function Home() {
                           >
                             Go Back
                           </button>
-                          
                         </div>
                       </Dialog.Panel>
                     </Transition.Child>
