ab8e6c3c88eb05f0a5c4a3f4a0e4d7ba384753e2
Satyam Bansal <sbansal1999@gmail.com>
2023-08-20 11:32:22 +0530

    Changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,24 +69,27 @@ export default function Home() {
 
   const tradeMutation = trpc.trades.create.useMutation();
 
-  const userQuery = trpc.users.getUserByID;
-
-  // Initial fetching of userData
-  const { data: userData } = useQuery({
-    queryKey: ["userData"],
-    queryFn: fetchUserData,
+  const { data: userIDFromLocalStorage } = useQuery({
+    queryKey: ["userID"],
+    queryFn: fetchUserIDFromLocalStorage,
   });
 
-  function fetchUserData() {
-    const dummyData: UserData = {
-      firstName: "John",
-      lastName: "Wick",
-      balance: 10000,
-      email: "john@wick.com",
-      id: "818cf69e-b17c-41e8-929c-01b79b502f49",
-    };
+  // fetching user data from DB
+  // TODO: ideally should be stored in context
+  const { data: userData, isError } = trpc.users.getUserByID.useQuery(
+    userIDFromLocalStorage || "",
+    {
+      refetchOnWindowFocus: false,
+      enabled: Boolean(userIDFromLocalStorage),
+    }
+  );
 
-    return dummyData;
+  function fetchUserIDFromLocalStorage() {
+    // simulates fetching user from local storage
+    const userIDFetchedFromLocalStorage =
+      "8a0a7f28-f7f5-47bb-800c-bfad230fffde";
+
+    return userIDFetchedFromLocalStorage;
   }
 
   const handleBuy = (e: FormEvent) => {
@@ -134,24 +137,17 @@ export default function Home() {
   };
 
   const handleConfirmOrder = async () => {
-    if (!selectedStock || !selectedStockQuote) {
-      return;
-    }
-
-    if (!userData) {
+    if (!selectedStock || !selectedStockQuote || !userData) {
       return;
     }
 
     const latestQuote = await fetchStockQuote(selectedStock.ticker);
-
     const orderValue = latestQuote.c * quantity;
-
     if (orderValue > userData.balance) {
       setShowInsufficientFundsModal(true);
       setShowConfirmOrderModal(false);
       return;
     }
-
     tradeMutation.mutate(
       {
         symbol: selectedStock.ticker,
@@ -167,7 +163,6 @@ export default function Home() {
         },
       }
     );
-
     setShowConfirmOrderModal(false);
   };
 
@@ -339,12 +334,10 @@ export default function Home() {
         <div className="m-5" />
 
         <div className="m-5 flex">
-          {userData && (
-            <PriceCard text="Current Balance" value={userData.balance} />
-          )}
+          <PriceCard text="Current Balance" value={userData?.balance} />
         </div>
 
-        {JSON.stringify(userData)}
+        {JSON.stringify(userIDFromLocalStorage)}
         <br />
         {JSON.stringify(selectedStock)}
         <br />
@@ -509,114 +502,6 @@ export default function Home() {
   );
 }
 
-const StocksListModal = ({
-  stocks,
-  showModal,
-  setShowModal,
-  setSelectedStock,
-  setSelectedStockQuote,
-}: ShowStocksModalProps) => {
-  if (stocks.length === 0) {
-    return <div>No Stock Found</div>;
-  }
-
-  // setSelectedStock(null);
-  // setSelectedStockQuote(null);
-
-  const handleStockSelect = async (idx: number) => {
-    setSelectedStock(stocks[idx]);
-    setShowModal(false);
-
-    const quote = await fetchStockQuote(stocks[idx].ticker);
-
-    setSelectedStockQuote(quote);
-  };
-
-  return (
-    <>
-      <Transition.Root show={showModal} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={() => setShowModal(false)}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-10">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-2/5 sm:max-w-max">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
-                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <PresentationChartLineIcon className="w-6" />
-                      </div>
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <Dialog.Title
-                          as="h3"
-                          className="text-lg font-medium leading-6 text-gray-900"
-                        >
-                          <p className="text-xl">
-                            These are the results for the search.
-                            <br />
-                            Select the stock you would like to trade.
-                          </p>
-                        </Dialog.Title>
-                        <Dialog.Description className="w-fw mt-5" as="div">
-                          <div className="mt-2 max-h-[60vh] overflow-auto rounded-md">
-                            {stocks.map((stock, idx) => (
-                              <button
-                                className="mt-1 w-full bg-slate-300 p-5 text-left hover:bg-slate-600"
-                                key={idx}
-                                onClick={() => {
-                                  void handleStockSelect(idx);
-                                }}
-                              >
-                                {stock.ticker + " - " + stock.name}
-                              </button>
-                            ))}
-                          </div>
-                        </Dialog.Description>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Go Back
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
-    </>
-  );
-};
-
 const ShowQuote: React.FC<{ quote: StockQuote }> = ({ quote }) => {
   const timeStamp = new Date(quote.t * 1000);
   const timeStampInEST =
@@ -667,34 +552,11 @@ const ShowQuote: React.FC<{ quote: StockQuote }> = ({ quote }) => {
   );
 };
 
-const Spinner = () => (
-  <svg
-    className="ml-2 mr-3 h-9 w-8 animate-spin text-white"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    ></circle>
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    ></path>
-  </svg>
-);
-
 const PriceCard: React.FC<{
   text: string;
   value?: number;
-  noDollars?: boolean;
-}> = ({ text, value, noDollars }) => (
+  noDollarSign?: boolean;
+}> = ({ text, value, noDollarSign: noDollars }) => (
   <div className="flex rounded-md border-2 p-4 shadow-md ">
     <div className="text-xl font-semibold">{text}</div>
     {value && (
@@ -710,63 +572,6 @@ const Card: React.FC<{ text: string }> = ({ text }) => (
     <div className="text-3xl font-semibold text-gray-700">{text}</div>
   </div>
 );
-
-const fetchStockSearchData = async (searchQuery: string) => {
-  // TODO: handle rate-limit case
-  const POLYGON_KEY = env.NEXT_PUBLIC_POLYGON_KEY;
-  const resultLimit = 10;
-
-  const reference = referenceClient(POLYGON_KEY);
-
-  const queryObject: ITickersQuery = {
-    type: "CS",
-    market: "stocks",
-    search: searchQuery,
-    active: "true",
-    limit: resultLimit,
-  };
-
-  const promiseNasdaq = reference.tickers({ ...queryObject, exchange: "XNAS" });
-  const promiseNYSE = reference.tickers({ ...queryObject, exchange: "XNYS" });
-
-  // eslint-disable-next-line no-useless-catch
-  try {
-    const [dataNasdaq, dataNYSE] = await Promise.all([
-      promiseNasdaq,
-      promiseNYSE,
-    ]);
-    const data = [...dataNasdaq.results, ...dataNYSE.results];
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const fetchStockQuote = async (stockTicker: string) => {
-  const FINNHUB_KEY = env.NEXT_PUBLIC_FINNHUB_KEY;
-
-  const stockQuoteQuery = `https://finnhub.io/api/v1/quote?symbol=${stockTicker}&token=${FINNHUB_KEY}`;
-
-  const stockQuoteQueryResponse = await fetch(stockQuoteQuery);
-  const queryJSON = (await stockQuoteQueryResponse.json()) as StockQuote;
-
-  // convert dollars into cents
-  queryJSON.c = getCents(queryJSON.c);
-  queryJSON.h = getCents(queryJSON.h);
-  queryJSON.l = getCents(queryJSON.l);
-  queryJSON.o = getCents(queryJSON.o);
-  queryJSON.pc = getCents(queryJSON.pc);
-
-  return queryJSON;
-};
-
-const formatToEST = (seconds: number) => {
-  const date = new Date(seconds * 1000);
-  const dateInEST = date.toLocaleString("en-US", {
-    timeZone: "America/New_York",
-  });
-  return dateInEST;
-};
 
 const RateLimitModal: React.FC<{
   showRateLimitModal: boolean;
@@ -927,3 +732,191 @@ const InsufficientFundsModal: React.FC<{
     </Transition.Root>
   </>
 );
+
+const StocksListModal = ({
+  stocks,
+  showModal,
+  setShowModal,
+  setSelectedStock,
+  setSelectedStockQuote,
+}: ShowStocksModalProps) => {
+  if (stocks.length === 0) {
+    return <div>No Stock Found</div>;
+  }
+
+  // setSelectedStock(null);
+  // setSelectedStockQuote(null);
+
+  const handleStockSelect = async (idx: number) => {
+    setSelectedStock(stocks[idx]);
+    setShowModal(false);
+
+    const quote = await fetchStockQuote(stocks[idx].ticker);
+
+    setSelectedStockQuote(quote);
+  };
+
+  return (
+    <>
+      <Transition.Root show={showModal} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setShowModal(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-2/5 sm:max-w-max">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <PresentationChartLineIcon className="w-6" />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-lg font-medium leading-6 text-gray-900"
+                        >
+                          <p className="text-xl">
+                            These are the results for the search.
+                            <br />
+                            Select the stock you would like to trade.
+                          </p>
+                        </Dialog.Title>
+                        <Dialog.Description className="w-fw mt-5" as="div">
+                          <div className="mt-2 max-h-[60vh] overflow-auto rounded-md">
+                            {stocks.map((stock, idx) => (
+                              <button
+                                className="mt-1 w-full bg-slate-300 p-5 text-left hover:bg-slate-600"
+                                key={idx}
+                                onClick={() => {
+                                  void handleStockSelect(idx);
+                                }}
+                              >
+                                {stock.ticker + " - " + stock.name}
+                              </button>
+                            ))}
+                          </div>
+                        </Dialog.Description>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Go Back
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+};
+
+const Spinner = () => (
+  <svg
+    className="ml-2 mr-3 h-9 w-8 animate-spin text-white"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
+  </svg>
+);
+
+const formatToEST = (seconds: number) => {
+  const date = new Date(seconds * 1000);
+  const dateInEST = date.toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  });
+  return dateInEST;
+};
+
+const fetchStockSearchData = async (searchQuery: string) => {
+  // TODO: handle rate-limit case
+  const POLYGON_KEY = env.NEXT_PUBLIC_POLYGON_KEY;
+  const resultLimit = 10;
+
+  const reference = referenceClient(POLYGON_KEY);
+
+  const queryObject: ITickersQuery = {
+    type: "CS",
+    market: "stocks",
+    search: searchQuery,
+    active: "true",
+    limit: resultLimit,
+  };
+
+  const promiseNasdaq = reference.tickers({ ...queryObject, exchange: "XNAS" });
+  const promiseNYSE = reference.tickers({ ...queryObject, exchange: "XNYS" });
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const [dataNasdaq, dataNYSE] = await Promise.all([
+      promiseNasdaq,
+      promiseNYSE,
+    ]);
+    const data = [...dataNasdaq.results, ...dataNYSE.results];
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchStockQuote = async (stockTicker: string) => {
+  const FINNHUB_KEY = env.NEXT_PUBLIC_FINNHUB_KEY;
+
+  const stockQuoteQuery = `https://finnhub.io/api/v1/quote?symbol=${stockTicker}&token=${FINNHUB_KEY}`;
+
+  const stockQuoteQueryResponse = await fetch(stockQuoteQuery);
+  const queryJSON = (await stockQuoteQueryResponse.json()) as StockQuote;
+
+  // convert dollars into cents
+  queryJSON.c = getCents(queryJSON.c);
+  queryJSON.h = getCents(queryJSON.h);
+  queryJSON.l = getCents(queryJSON.l);
+  queryJSON.o = getCents(queryJSON.o);
+  queryJSON.pc = getCents(queryJSON.pc);
+
+  return queryJSON;
+};
